6f490da15111c87f280c1d141d6c6149c45a3741
unknown <sidu2911@gmail.com>
2025-03-02 22:29:35 +0530

    gitignore update and process env

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,14 @@ import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
 import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND,
     methods: ["GET", "POST"]
   }
 });
@@ -16,14 +18,15 @@ const io = new Server(server, {
 app.use(cors());
 app.use(express.json());
 
+const SEND_MESSAGE = "send_message";
+const RECEIVE_MESSAGE = "receive_message";
 
 io.on("connection", (socket) => {
   console.log("A user connected: ", socket.id);
 
-  socket.on("send_message", (data) => {
+  socket.on(SEND_MESSAGE, (data) => {
     console.log("Received message: ", data);
-    io.emit("message", data);
-    io.emit("receive_message", data);
+    io.emit(`${RECEIVE_MESSAGE}`, data);
   });
 
   socket.on("disconnect", () => {
